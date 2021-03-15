cfeb8a426c6b01a5f1e3b01bc63aa756c8d02dcc
victor ekstrom <victor.ekstrom@vertel.se>
2021-03-15 13:03:41 +0000

    snyggat till koden lite

--- odoo_stock_availability/models/models.py
+++ odoo_stock_availability/models/models.py
@@ -4,7 +4,8 @@ from odoo import models, fields, api
 
 
 class ProductTemplate(models.Model):
-     _inherit = 'product.template'
-     
-     product_availability = fields.Selection([("in_stock", "In stock"), ("limited", "Limited"), ("out_of_stock", "Out of stock")], default="in_stock", required=True)
+    _inherit = 'product.template'
 
+    product_availability = fields.Selection([("in_stock", "In stock"),
+        ("limited", "Limited"), ("out_of_stock", "Out of stock")],
+        default="in_stock", required=True)
