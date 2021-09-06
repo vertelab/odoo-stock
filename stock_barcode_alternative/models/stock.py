2a0394b1281d815aace3f935f15a8739298c8880
victor ekstrom <victor.ekstrom@vertel.se>
2021-09-06 12:20:52 +0000

    stock_barcode_alternative: first working version for 14.0

---
+++ stock_barcode_alternative/models/stock.py
@@ -0,0 +1,260 @@
+﻿from odoo import api, fields, models, _
+from odoo import http
+from odoo.http import request
+from odoo.tools import safe_eval as eval
+import traceback
+
+from odoo.modules.registry import Registry
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class BarcodeController(http.Controller):
+
+    @http.route(['/barcode2/web/'], type='http', auth='user')
+    def abc_interface(self, debug=False, **k):
+        if not request.session.uid:
+            return http.local_redirect('/web/login?redirect=/barcode2/web')
+
+        return request.render('stock_barcode_alternative.barcode_index')
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.model
+    def abc_make_records(self, records, fields=None):
+        """Build a record description for return to the JS interface."""
+        fields = fields or self.abc_get_model_fields(records)
+        result = []
+        field_types = {}
+
+        def field_type(name):
+            """Check the type of a field."""
+            if name not in field_types:
+                field_types[name] = records.fields_get([name], attributes=['type'])[name]['type']
+            return field_types.get(name)
+        for record in records:
+            rec = {'_name': record._name, 'id': record.id}
+            for field in fields:
+                child_fields = None
+                # Relational field
+                if type(field) == tuple:
+                    child_fields = field[1] or ['display_name']
+                    field = field[0]
+                value = getattr(record, field)
+                if child_fields:
+                    value = self.abc_make_records(value, child_fields)
+                    if field_type(field) == 'many2one':
+                        value = value and value[0] or None
+                rec[field] = value
+            result.append(rec)
+        return result
+
+    @api.model
+    def abc_get_model_fields(self, record):
+        """Return a list of the fields that should be loaded for this model."""
+        if record._name == 'stock.picking':
+            return [
+                'name',
+                ('product_id', ['sale_ok']),
+                'state',
+                ('partner_id', ['display_name']),
+            ]
+        if record._name == 'stock.transfer_details_items':
+            return [
+                    ('product_id', ['display_name']),
+                    ('product_uom_id', ['display_name', 'factor']),
+                    'quantity',
+                    ('package_id', []),
+                    ('packop_id', []),
+                    ('result_package_id', ['display_name']),
+                    # ('sourceloc_id', ['display_name']),
+                    ('destinationloc_id', ['display_name']),
+                    ('lot_id', ['display_name']),
+                ]
+        if record._name == 'stock.move.line':
+            return [
+                    ('product_id', ['display_name']),
+                    ('product_uom_id', ['display_name', 'factor']),
+                    'product_uom_qty',
+                    'product_qty',
+                    'qty_done',
+                    ('location_id', ['display_name']),
+                    ('location_dest_id', ['display_name']),
+                ]
+        if record._name == 'stock.move':
+            return [
+                    ('product_id', ['display_name']),
+                    ('product_uom', ['display_name', 'factor']),
+                    'product_uom_qty',
+                    'product_qty',
+                    'reserved_availability',
+                    'availability',
+                    'quantity_done',
+                    'forecast_availability',
+                    ('location_id', ['display_name']),
+                    ('location_dest_id', ['display_name']),
+                ]
+        if record._name == 'stock.quant.package':
+            return [
+                    'name',
+                    ('location_id', ['display_name']),
+                    ('packaging_id', ['display_name']),
+                ]
+        if record._name == 'product.product':
+            return [
+                    'display_name',
+                    'default_code',
+                    'barcode',
+                    'sale_ok',
+                    'weight',
+                    ('uom_id', ['display_name', 'factor']),
+                ]
+        if record._name == 'stock.location':
+            return [
+                    'display_name',
+                ]
+        if record._name == 'product.uom':
+            return [
+                    'display_name',
+                    'factor',
+                ]
+        if record._name == 'product.packaging':
+            return [
+                    'name',
+                    'height',
+                    'width',
+                    'packaging_length',
+                    'weight',
+                    'max_weight',
+                    'barcode'
+                ]
+        return ['id']
+
+    def abc_load_picking(self, picking_id):
+        """Create a JSON description of a picking and its products for the Javascript GUI."""
+        stock_picking_id = self.env['stock.picking'].browse(picking_id)
+        picking = self.abc_make_records(stock_picking_id)
+        if stock_picking_id.state == 'assigned':
+            operations = self.abc_make_records(stock_picking_id.move_line_ids)
+            products = self.abc_make_records(stock_picking_id.move_line_ids.mapped("product_id"))
+        else:
+            operations = []
+            products = []
+        # TODO: Find packages
+        packages = []
+        res = {'picking': picking, 'operations': operations, 'products': products, 'packages': packages}
+        return res
+
+    def abc_do_transfer(self, lines, packages, **data):
+        """Complete the picking operation."""
+        res = {'warnings': [], 'messages': [], 'results': {}}
+        params = {}
+        for step in [s[1] for s in sorted(self.abc_transfer_steps())]:
+            # Perform all the necessary picking steps
+            # lines     Line data from UI
+            # packages  Package data from UI
+            # data      Other data from UI
+            # params    Parameters accumulated in the picking process. Inject data communicated between steps here.
+            # res       The result returned to the UI.
+            getattr(self, step)(lines, packages, data, params, res)
+        return res
+
+    def abc_create_row(self, row):
+        """Provide default locations and other data """
+        # Lifted from action_assign on stock.move
+        product = self.env['product.product'].browse(row['product_id'])
+        location = self.location_id
+        main_domain = [('reservation_id', '=', False), ('qty', '>', 0)]
+        quants = self.env['stock.quant'].quants_get_prefered_domain(
+            location,
+            product,
+            row['quantity'] or 1.0,
+            domain=main_domain,
+            prefered_domain_list=[])
+        # Find suggested location. Could be more than one given quantity > 0.
+        # All that stuff remains to be solved.
+        for quant in quants:
+            if quant[0]:
+                location = quant[0].location_id
+        row.update({
+            '_name': 'stock.transfer_detailsitems',
+            'product_id': self.abc_make_records(product, ['display_name'])[0],
+            'sale_ok' : self.abc_make_records(product, ['sale_ok'])[0],
+            'destinationloc_id': self.abc_make_records(self.location_dest_id)[0],
+            # 'sourceloc_id': self.abc_make_records(location)[0],
+            'product_uom_id': self.abc_make_records(product.uom_id)[0],
+        })
+        return row
+
+    @api.model
+    def abc_transfer_steps(self):
+        """Return all the steps (function names) to complete the picking process in the correct order."""
+        return [
+            (20, 'abc_transfer_wizard'),
+            (40, 'abc_create_invoice'),
+            (60, 'abc_confirm_invoice')
+        ]
+
+    def abc_transfer_wizard(self, lines, packages, data, params, res):
+        """Run the transfer wizard on the given lines."""
+
+        for move in lines:
+            self.env["stock.move.line"].browse(move['id']).qty_done = move['qty_done']
+        action = self.button_validate()
+        # Keep track of matched transfer items
+        res['results']['transfer'] = 'success'
+        params['wizard'] = action
+
+    def abc_create_invoice(self, lines, packages, data, params, res):
+        # ~ TODO: we should create an invoice here!!!! This is a needed functionality that needs to be implemented before going to production
+        # ~ _logger.warning(f"victor: {self.sale_id._create_invoices()}")
+        pass
+
+    def abc_confirm_invoice(self, lines, packages, data, params, res):
+        """Confirm invoice. Split into its own function to not lock the invoice sequence."""
+        # ~ TODO: we should confirm an invoice here!!!! This is a needed functionality that needs to be implemented before going to production
+        pass
+
+    def abc_open_picking(self):
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': '/barcode2/web#picking_id=%s' % self.id,
+        }
+
+    @api.model
+    def abc_scan(self, code):
+        """Perform scan on the supplied barcode."""
+        products = self.env['product.product'].search(['|', ('barcode', '=', code), ('default_code', '=', code)])
+        if products:
+            return {
+                'type': 'product.product',
+                'product': self.abc_make_records(products)}
+        picking = self.env['stock.picking'].search_read([('name', '=', code)], ['id'])
+        # the barcode scanner isnt capable of giving us "/", when it reads a "/" it gives us a "-"
+        if not picking:
+            picking = self.env['stock.picking'].search_read([('name', '=', code.replace('-', '/'))], ['id'])
+        if picking:
+            return {
+                'type': 'stock.picking',
+                'picking': picking[0]
+            }
+        package = self.env['product.packaging'].search([('barcode', '=', code)])
+        return {
+                'type': 'product.packaging',
+                'package': self.abc_make_records(package)
+            }
+        return {'type': 'no hit'}
+
+class StockPickingType(models.Model):
+    _inherit = 'stock.picking.type'
+
+    def abc_open_barcode_interface(self):
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'self',
+            'url': '/barcode2/web',
+        }
---
+++ stock_barcode_alternative_unifaun/static/src/js/widgets.js
@@ -0,0 +1,198 @@
+odoo.define('stock_barcode_alternative.Unifaun', function(require) {
+    "use strict";
+
+    var publicWidget = require("web.public.widget");
+    var BarcodeInterface = require('stock_barcode_alternative.BarcodeInterface');
+    var PickingEditorWidget = require('stock_barcode_alternative.PickingEditorWidget');
+    var xml_files = BarcodeInterface.prototype.xmlDependencies.concat(['/stock_barcode_alternative_unifaun/static/src/xml/picking.xml'])
+    BarcodeInterface.include({
+        xmlDependencies: xml_files
+    });
+    
+class Package {
+                constructor(weight, name, id){
+                this.id = id
+                this.name = name;
+                this.weight = weight;
+                }
+            }
+    
+    PickingEditorWidget.include({
+        init: function(parent, options){
+            this.manual_packages = []
+            this.display_manual_packages = []
+            var pack = new Package(0, `Package ${this.manual_packages.length + 1}`, this.manual_packages.length);
+            this.manual_packages.push(pack)
+            this.display_manual_packages = this.manual_packages
+            this._super(parent, options);
+            let unifaun_order = null;
+            let unifaun_group = false;
+            this.unifaun_parcel_count = 1;
+            if (parent){
+                unifaun_order = parent.unifaun_id;
+            }
+            if (unifaun_order && unifaun_order.picking_ids && unifaun_order.picking_ids.length > 1) {
+                unifaun_group = true;
+            }
+            if (this.unifaun_no_order == null){
+                if (unifaun_group){
+                    this.unifaun_no_order = true;
+                } else {
+                    this.unifaun_no_order = false;
+                }
+            }
+            if (this.unifaun_parcel_count == null){
+                this.unifaun_parcel_count = 0;
+            }
+            if (this.unifaun_parcel_weight == null){
+                this.unifaun_parcel_weight = 0.0;
+            }
+            
+            
+        },
+        renderElement: function(){
+            var self = this;
+            this._super();
+            this.$('i.package-qty-minus').click(() => {
+                // Uses the following: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/stepDown
+                let package_qty = this.$("#abc_dn_unifaun_nr_packages")[0];
+                package_qty.stepDown()
+                self.unifaun_update_packages()
+            })
+            this.$('i.package-qty-plus').click(() => {  
+                // Uses the following: https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/stepUp   
+                let package_qty =  this.$("#abc_dn_unifaun_nr_packages")[0];
+                package_qty.stepUp()
+                self.unifaun_update_packages()
+            })
+            this.$('i.total-weight-minus').click(() => {     
+                let package_weight = this.$("#abc_dn_unifaun_weight")[0];
+                package_weight.stepDown()
+                self.unifaun_update_packages()
+            })
+            this.$('i.total-weight-plus').click(() => {     
+                let package_weight = this.$("#abc_dn_unifaun_weight")[0];
+                package_weight.stepUp()
+                self.unifaun_update_packages()
+            })
+            this.$('i.unique-package-weight-minus').click((c) => {
+                let targetbutton = c.target.attributes['button-id'].value
+                this.$('.unifaun_package_weight_input').each((i, n) => {
+                    if (targetbutton == n.attributes['list-id'].value) {
+                        n.stepDown()
+                        self.unifaun_update_packages()
+                    }
+                })
+            })
+            this.$('i.unique-package-weight-plus').click((c) => {     
+                let targetbutton = c.target.attributes['button-id'].value
+                this.$('.unifaun_package_weight_input').each((i, n) => {
+                    if (targetbutton == n.attributes['list-id'].value) {
+                        n.stepUp()
+                        self.unifaun_update_packages()
+                    }
+                })
+            })
+
+            this.$('#abc_dn_unifaun_active').change(function(){self.unifaun_update_packages()});
+            this.$('#abc_dn_unifaun_nr_packages').change(function(){self.unifaun_update_packages()});
+            this.$('#abc_dn_unifaun_weight').change(function(){self.unifaun_update_packages()});
+            this.$('.unifaun_package_weight_input').change(function(){self.unifaun_update_packages()});
+        },
+
+        get_saved_fields: function() {
+            var fields = this._super();
+            return fields.concat(['unifaun_no_order', 'unifaun_parcel_count', 'unifaun_parcel_weight'])
+        },
+
+        unifaun_update_packages: function() {    
+            
+            let self = this
+            
+            // Update Unifaun data when changes are made in GUI.
+            var suppress = this.$('#abc_dn_unifaun_active').prop('checked');
+            if (suppress) {
+                var parcel_count = 0;
+                var parcel_weight = 0.0;
+            } else {
+                var parcel_count = parseInt(this.$('#abc_dn_unifaun_nr_packages').val()) || 0;
+                var parcel_weight = parseFloat(this.$('#abc_dn_unifaun_weight').val()) || 0.0;
+                var package_weights = []
+                this.$('.unifaun_package_weight_input').each(function(){
+                    package_weights.push($(this).val());
+                    })
+
+            }
+            for (var key = 0; key < package_weights.length; key++) {
+                self.manual_packages[key].weight = package_weights[key]
+                self.save('manual_packages')
+            }
+            if (parcel_count > this.manual_packages.length) {
+                    for (var i = 0; i < parcel_count - package_weights.length; i++) {
+                        var pack = new Package(0, `Package ${this.manual_packages.length + 1}`, this.manual_packages.length);
+                        this.manual_packages.push(pack)
+                    }
+                    this.display_manual_packages = this.manual_packages
+                    this.save('manual_packages')
+                    this.save('display_manual_packages')
+                }
+            else if (parcel_count < this.manual_packages.length) {
+                this.display_manual_packages = this.manual_packages.slice(0, parcel_count)
+                this.save('display_manual_packages')
+            }
+            if (suppress !== this.unifaun_no_order) {
+                this.unifaun_no_order = suppress;
+                this.save('unifaun_no_order');
+            }
+            if (parcel_count !== this.unifaun_parcel_count) {
+                this.unifaun_parcel_count = parcel_count;
+                this.save('unifaun_parcel_count');
+            }
+            if (parcel_weight !== this.unifaun_parcel_weight) {
+                this.unifaun_parcel_weight = parcel_weight;
+                this.save('unifaun_parcel_weight');
+            }
+            this.renderElement();
+        },
+        get_extra_transfer_data: function(){
+            var self = this;
+            var data = this._super();
+            _.each(['unifaun_no_order', 'unifaun_parcel_count', 'unifaun_parcel_weight'], function(field){
+                data[field] = self[field];
+            });
+            return data;
+        },
+        do_transfer: function(){
+            if (! this.getParent().is_unifaun || this.unifaun_no_order || (this.unifaun_parcel_count > 0 && this.unifaun_parcel_weight > 0)) {
+                return this._super();
+            };
+            this.getParent().log_message("You must set # of packages and weight to complete a Unifaun order.", 'warning');
+        },
+        toggle_ui_elements: function(){
+            // Update UI when changes are made to Unifaun data.
+            var el_weight = this.$('#dn_wrapping_weight_calculated');
+            el_weight.text(this.get_weight().toFixed(3));
+            return this._super();
+        },
+        transfer_button_disabled: function(){
+            // Check if transfer button should be disabled.
+            var suppress = this.$('#abc_dn_unifaun_active').prop('checked');
+            var parcel_count = parseInt(this.$('#abc_dn_unifaun_nr_packages').val()) || 0;
+            var parcel_weight = parseFloat(this.$('#abc_dn_unifaun_weight').val()) || 0.0;
+            if (!(! this.getParent().is_unifaun || this.unifaun_no_order || (this.unifaun_parcel_count > 0 && this.unifaun_parcel_weight > 0))){
+                return true;
+            }
+            return this._super();
+        },
+        rest_order_button_disabled: function(tb_disabled){
+            // Check if transfer button should be disabled.
+            var suppress = this.$('#abc_dn_unifaun_active').prop('checked');
+            var parcel_count = parseInt(this.$('#abc_dn_unifaun_nr_packages').val()) || 0;
+            var parcel_weight = parseFloat(this.$('#abc_dn_unifaun_weight').val()) || 0.0;
+            if (!(! this.getParent().is_unifaun || this.unifaun_no_order || (this.unifaun_parcel_count > 0 && this.unifaun_parcel_weight > 0))){
+                return true;
+            }
+            return this._super(tb_disabled);
+        }
+    });
+});
