1025fbfb3b2b562bbaceb42c5bf7a00fa58256de
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-06 11:58:38 +0100

    Update __manifest__.py

--- stock_location_priority/__manifest__.py
+++ stock_location_priority/__manifest__.py
@@ -1,31 +1,51 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "stock_location_priority",
-
-    'summary': """
-        Added a fipo rule that allows prioritization of locations
-        """,
-
-    'description': """
-        Added a fipo rule that allows prioritization of locations
-    """,
-
-    'author': "Vertel AB",
-    'website': "vertel.se",
-
+    'name': 'Stock: Location Priority',
+    'version': '14.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Added a fipo rule that allows prioritization of locations.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'stock',
-    'version': '14.0.0.0.1',
+    'category': 'Stock',
+    'description': """
+    Added a fipo rule that allows prioritization of locations.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-stock/stock_location_priority',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-stock',
+    # Any module necessary for this one to work correctly
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'stock'],
-
     # always loaded
     'data': [
         'data/data.xml',
         'views/views.xml',
     ],
-
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
