28eca37c2fb41f65657bc0d45d3d425a2c603d74
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-06 11:07:49 +0100

    Update __manifest__.py

--- picking_reservation/__manifest__.py
+++ picking_reservation/__manifest__.py
@@ -1,25 +1,47 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "picking_reservation",
-
-    'summary': """
-    When a locations need to use a pull move to satiate an order, now it won't reserve the exact products moved. It will go after fifo or lifo for all products.
-    """,
-
-    'description': """
-        When a locations need to use a pull move to satiate an order, now it won't reserve the exact products moved. It will go after fifo or lifo for all products.
-    """,
-
-    'author': "Vertel AB",
-    'website': "https://vertel.se",
-
+    'name': 'Stock: Picking Reservation',
+    'version': '14.0.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'When a locations need to use a pull move to satiate an order, now it won''t reserve the exact products moved. It will go after fifo or lifo for all products.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Stock',
-    'version': '14.0.0.0.1',
+    'description': """
+    When a locations need to use a pull move to satiate an order, now it won''t reserve the exact products moved. It will go after fifo or lifo for all products.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-stock/calc_orderpoint',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-stock',
+    # Any module necessary for this one to work correctly
 
-    # any module necessary for this one to work correctly
     'depends': ['base','stock'],
 
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
