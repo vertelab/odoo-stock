7e9e2600406262133effada8290af6ed94313c7d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-06 12:01:53 +0100

    Update __manifest__.py

--- stock_multiple_picker/__manifest__.py
+++ stock_multiple_picker/__manifest__.py
@@ -1,35 +1,47 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# OpenERP, Open Source Management Solution, third party addon
-# Copyright (C) 2016- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 {
-    'name': 'Stock Multiple Pickers',
-    'version': '0.2',
-    'summary': 'Extends Picking list with pickers',
-    'category': 'stock',
+    'name': 'Stock: Multiple Picker',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Extends Picking list with pickers.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Stock',
     'description': """
-        Wizard that distributes one or many pickers on a picking. Multiple persons can pick
-        in parallell. Financed by Dermanord-Svensk Hudvård AB
+    Wizard that distributes one or many pickers on a picking. Multiple persons can pick
+    in parallell. Financed by Dermanord-Svensk Hudvård AB
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-stock/stock_multiple_picker',
+    'images': ['static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
-    'website': 'http://www.vertel.se',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-stock',
+    # Any module necessary for this one to work correctly
+
     'depends': ['stock', 'hr', 'base_report_to_printer', 'delivery'],
     'data': [
         'security/ir.model.access.csv',
@@ -39,3 +51,4 @@
     ],
     'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
