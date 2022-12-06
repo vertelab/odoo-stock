56dd77221a20ffe26602dcdfad7387520f960e31
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-06 11:23:57 +0100

    Update __manifest__.py

--- stock_barcode_alternative_unifaun/__manifest__.py
+++ stock_barcode_alternative_unifaun/__manifest__.py
@@ -1,17 +1,48 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': 'Stock Barcode Alternative - Unifaun',
+    'name': 'Stock: Barcode Alternative -- Unifaun',
     'version': '14.0.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Unifaun functionality for the alternative barcode scanning interface.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sign',
     'description': """
 Stock Barcode Alternative Unifaun
 ================================
 Adds Unifaun functionality to the alternative barcode interface.
     """,
-    'category': 'website',
-    'website': 'http://www.vertel.se',
+    #'sequence': '1',
     'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-stock/stock_barcode_alternative_unifaun',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-stock',
+    # Any module necessary for this one to work correctly
+
     'depends': ['stock_barcode_alternative', 'delivery_unifaun_base'],
     'data': [
         'views/stock_view.xml',
@@ -22,3 +53,4 @@ Adds Unifaun functionality to the alternative barcode interface.
     'application': True,
     'auto_install': False,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
