280340745be47ca21e354b90d400c2e9bb749c24
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-12-06 11:45:06 +0100

    Update __manifest__.py

--- stock_customer_type/__manifest__.py
+++ stock_customer_type/__manifest__.py
@@ -1,27 +1,48 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "odoo-stock-picking",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "Vertel AB",
-    'website': "https://vertel.se",
-
+    'name': 'Stock: Customer Type',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': ' Short (1 phrase/line) summary of the module''s purpose, used as subtitle on modules listing or apps.openerp.com',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Stock',
+    'description': """
+    Short (1 phrase/line) summary of the module's purpose, used as
+    subtitle on modules listing or apps.openerp.com
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-stock/stock_barcode_dermanord',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-stock',
+    # Any module necessary for this one to work correctly
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'stock', 'sale', 'product'],
-
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
@@ -33,3 +54,4 @@
         'demo/demo.xml',
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
