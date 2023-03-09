38a299bdd6db29db1a93e2483a1196144edfc66a
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-03-09 09:05:21 +0100

    Update __manifest__.py
    minor spelling error

--- stock_customer_type/__manifest__.py
+++ stock_customer_type/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Stock: Customer Type',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': ' Short (1 phrase/line) summary of the module''s purpose, used as subtitle on modules listing or apps.openerp.com',
+    'summary': ' Short (1 phrase/line) summary of the module''s purpose, used as subtitle on modules listing or apps.odoo.com',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
